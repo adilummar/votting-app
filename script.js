6ef638d4591ec51c1d6163bfdbe08084c4e1f2a2
adilummar <madilummer@gmail.com>
2023-05-31 05:57:36 +0530

    scripting started

---
+++ script.js
@@ -0,0 +1,13 @@
+// assignation 
+const candidates = document.querySelectorAll(".candidante-box")
+
+
+
+
+
+// event  
+candidates.forEach(item=>{
+    item.addEventListener('click',()=>{
+        item.
+    })
+})
