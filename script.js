f5ca68b8592750d2d34c9a67b45550a12d48aa6a
adilummar <madilummer@gmail.com>
2023-05-31 07:18:31 +0530

    starting funcitoning permit

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
 // assignation
 const candidates = document.querySelectorAll(".candidante-box");
+const limitBtn = document.querySelector(".s-btn");
+const limitCount = document.getElementById("limit");
 
 // variables
+var limit = 0;
 const candidate = [
   {
     name: "safwan kunichira",
-    vote : 0, 
-    setVote : function(){
-        this.vote = this.vote + 1 
-    }
+    vote: 0,
+    setVote: function () {
+      this.vote = this.vote + 1;
+    },
   },
   {
     name: "murshid munderi",
-    vote : 0, 
-    setVote : function(){
-        this.vote = this.vote + 1 
-    }
+    vote: 0,
+    setVote: function () {
+      this.vote = this.vote + 1;
+    },
   },
   {
     name: "midlaj koyath",
-    vote : 0, 
-    setVote : function(){
-        this.vote = this.vote + 1 
-    }
+    vote: 0,
+    setVote: function () {
+      this.vote = this.vote + 1;
+    },
   },
   {
     name: "basil kuranmukke",
-    vote : 0, 
-    setVote : function(){
-        this.vote = this.vote + 1 
-    }
+    vote: 0,
+    setVote: function () {
+      this.vote = this.vote + 1;
+    },
   },
 ];
 
+
 // event
+limitBtn.addEventListener('click',()=>{
+    if(!limitCount.value == 0){
+        limit = limitCount.value
+        limitCount.value = ''
+        console.log(limit)
+    }
+})
+
 candidates.forEach((item) => {
   item.addEventListener("click", () => {
     const nameDiv = item.querySelector(".candi-name").textContent;
-    for(let i = 0; i < 4 ; i++){
-        if(candidate[i].name === nameDiv){
-            candidate[i].setVote()
-            console.log(candidate[i].vote, candidate[i].name)
-        }
-         
+    for (let i = 0; i < 4; i++) {
+      if (candidate[i].name === nameDiv) {
+        candidate[i].setVote();
+        console.log(candidate[i].vote, candidate[i].name);
+      }
     }
   });
 });
+
+
